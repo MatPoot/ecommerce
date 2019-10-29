ff435ec34204b77dd4ef35ea2e4b5665d0a01de8
MatPoot <ch2shahid@gmail.com>
2019-10-29 12:47:46 -0600

    update our menu-items to use withRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import { Switch,Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import './pages/homepage/homepage-styles.scss';
 
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats test page</h1>
+  </div>
+)
+
+const TopicDetail = () => (
+  <div>
+    <h1>TopicDetail test page</h1>
+  </div>
+)
+
 function App() {
   return (
-  <div><HomePage></HomePage></div>
+    <div>
+      <Switch>
+     <Route exact path='/' component={HomePage} /> 
+     <Route path='/hats' component={HatsPage} /> 
+     <Route path='/topics/:topicid' component={TopicDetail} /> 
+     </Switch>
+    </div>
+  
   );
 }
 
