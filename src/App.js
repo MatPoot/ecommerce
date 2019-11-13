bc0b4fb7bf0002737a0db0f103abdb018a534021
MatPoot <ch2shahid@gmail.com>
2019-11-12 19:55:22 -0700

    added sign in to firebase with google

--- src/App.js
+++ src/App.js
@@ -1,35 +1,53 @@
-import React from 'react';
-import { Switch,Route } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
 
-import HomePage from './pages/homepage/homepage.component';
-import './pages/homepage/homepage-styles.scss';
-import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
-import SignInAndSignUpPage from './components/sign-in-and-sign-up/sign-in-and-sign-up.component';
-
+import HomePage from "./pages/homepage/homepage.component";
+import "./pages/homepage/homepage-styles.scss";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./components/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import { auth } from "./firebase/firebase.utils";
 
 const TopicDetail = () => (
   <div>
     <h1>TopicDetail test page</h1>
   </div>
-)
+);
 
-function App() {
-  return (
-    <div>
-      <Header></Header>
-      <Switch>
-     <Route exact path='/' component={HomePage} /> 
-     <Route path='/shop' component={ShopPage} /> 
-     <Route path='/signin' component={SignInAndSignUpPage} /> 
-     <Route path='/topics/:topicid' component={TopicDetail} /> 
-     </Switch>
-     
-    </div>
-  
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    };
+  }
+  unsubscribeFromAuth = null;
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser}></Header>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUpPage} />
+          <Route path="/topics/:topicid" component={TopicDetail} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
-
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,44 +1,62 @@
-import React from 'react';
+import React from "react";
 
-import FormInput from '../form-input/form-input.component';
+import FormInput from "../form-input/form-input.component";
 
-import './sign-in.styles.scss';
+import { signInwithGoogle } from "../../firebase/firebase.utils";
+
+import "./sign-in.styles.scss";
+import CustomButton from "../custom-button/custom-button.component";
 
 class SignIn extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            email: '',
-            password: ''
-        }
-    }
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
 
-    handleSubmit = event => {
-        event.preventDefault();
+  handleSubmit = event => {
+    event.preventDefault();
 
-        this.setState({email: '', password: ''})
-    }
-    handleChange = event => {
-        const {value,name} = event.target;
-        this.setState({[name]: value})
-    }
-    // handlechange function sets the name of the state property according to the name of the input tag, utilizing a single function for two operations. Hence why both 
+    this.setState({ email: "", password: "" });
+  };
+  handleChange = event => {
+    const { value, name } = event.target;
+    this.setState({ [name]: value });
+  };
+  // handlechange function sets the name of the state property according to the name of the input tag, utilizing a single function for two operations. Hence why both
 
-    render () {
+  render() {
     return (
-        <div className='sign-in'>
-            <h3>I have an account</h3>
-            <span>Sign in</span>
-            <form onSubmit={this.handleSubmit}>
-                <FormInput name="email" type="email" value={this.state.email} onChange={this.handleChange} required></FormInput>
-                <label>Email</label>
-                <FormInput name="password" type="password" value={this.state.password} onChange={this.handleChange} required ></FormInput>
-                <label>Password</label>
-                <FormInput type='submit' value='Submit'></FormInput>
-            </form>
-        </div>
-        );
-    }
+      <div className="sign-in">
+        <h3>I have an account</h3>
+        <span>Sign in</span>
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name="email"
+            type="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            required
+          ></FormInput>
+          <label>Email</label>
+          <FormInput
+            name="password"
+            type="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            required
+          ></FormInput>
+          <label>Password</label>
+          {/* <FormInput type='submit' value='Submit'></FormInput> */}
+          <CustomButton onClick={signInwithGoogle} isGoogleSignIn>
+            Google Sign-In
+          </CustomButton>
+        </form>
+      </div>
+    );
+  }
 }
 export default SignIn;
