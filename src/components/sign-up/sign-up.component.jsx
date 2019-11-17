e7a83a8cbbae4cb9fa0fb6940f248444d634968d
MatPoot <ch2shahid@gmail.com>
2019-11-17 13:17:21 -0700

    implemented and fixed sign in errors

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import "./sign-up.styles.scss";
 
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
 import { auth, createUserProfileDocument } from "../../firebase/firebase.utils";
+
+import "./sign-up.styles.scss";
 
 class SignUp extends React.Component {
   constructor() {
@@ -22,10 +23,12 @@ class SignUp extends React.Component {
     event.preventDefault();
 
     const { displayName, email, password, confirmPassword } = this.state;
+
     if (password !== confirmPassword) {
-      alert("The password do not match");
+      alert("passwords don't match");
       return;
     }
+
     try {
       const { user } = await auth.createUserWithEmailAndPassword(
         email,
@@ -33,8 +36,8 @@ class SignUp extends React.Component {
       );
 
       await createUserProfileDocument(user, { displayName });
+
       this.setState({
-        // clears form
         displayName: "",
         email: "",
         password: "",
@@ -55,9 +58,9 @@ class SignUp extends React.Component {
     const { displayName, email, password, confirmPassword } = this.state;
     return (
       <div className="sign-up">
-        <h2 className="title">No account?</h2>
-        <span>Sign up!</span>
-        <form className="sign-up-form" onSubmit={this.handlesubmit}>
+        <h2 className="title">I do not have a account</h2>
+        <span>Sign up with your email and password</span>
+        <form className="sign-up-form" onSubmit={this.handleSubmit}>
           <FormInput
             type="text"
             name="displayName"
@@ -65,23 +68,23 @@ class SignUp extends React.Component {
             onChange={this.handleChange}
             label="Display Name"
             required
-          ></FormInput>
+          />
           <FormInput
             type="email"
-            name="displayName"
+            name="email"
             value={email}
             onChange={this.handleChange}
             label="Email"
             required
-          ></FormInput>
+          />
           <FormInput
             type="password"
-            name="displayName"
+            name="password"
             value={password}
             onChange={this.handleChange}
-            label="password"
+            label="Password"
             required
-          ></FormInput>
+          />
           <FormInput
             type="password"
             name="confirmPassword"
@@ -89,8 +92,8 @@ class SignUp extends React.Component {
             onChange={this.handleChange}
             label="Confirm Password"
             required
-          ></FormInput>
-          <CustomButton type="submit">Confirm</CustomButton>
+          />
+          <CustomButton type="submit">SIGN UP</CustomButton>
         </form>
       </div>
     );
