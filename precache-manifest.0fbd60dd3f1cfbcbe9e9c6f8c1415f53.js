dc9e1da5aa40a20d4c290dccb6c27ed08509b32c
MatPoot <ch2shahid@gmail.com>
2020-02-20 14:39:30 -0700

    Updates

---
+++ precache-manifest.0fbd60dd3f1cfbcbe9e9c6f8c1415f53.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67dcbb405773a4c5426862020516cd00",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "242c805cb55a41881cc9",
+    "url": "/ecommerce/static/css/main.8f8bd6c9.chunk.css"
+  },
+  {
+    "revision": "1fceea86d94f1a5cda7e",
+    "url": "/ecommerce/static/js/2.e0992619.chunk.js"
+  },
+  {
+    "revision": "0052b61c75815ede2d3438c3cb42346b",
+    "url": "/ecommerce/static/js/2.e0992619.chunk.js.LICENSE"
+  },
+  {
+    "revision": "242c805cb55a41881cc9",
+    "url": "/ecommerce/static/js/main.23faf753.chunk.js"
+  },
+  {
+    "revision": "45747539ada40a19a388",
+    "url": "/ecommerce/static/js/runtime-main.eb7a6a53.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/ecommerce/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/ecommerce/static/media/shopping-bag.30f07071.svg"
+  }
+]);
